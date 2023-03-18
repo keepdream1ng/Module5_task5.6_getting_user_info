a0ec72a22f4cc0bd5dd71fe06d7557373d54f7ce
Anatolii Ivanov <gotnickname@gmail.com>
2023-03-18 06:57:40 +0530

    Add project files.

---
+++ Module5_task5.6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Module5_task5._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        // to do 
+        // IsItValidString
+        // GetValidString
+        // IsItValidNum
+        // GetValidNum
+        // IsItValidBool
+        // GetValidBool
+        // GetStrArrayFromConsole
+        // GetUserInfo
+        // PrintUserInfo
+        
+        //static bool IsItValidString (string str)
+        //{
+
+        //}
+    }
+}
